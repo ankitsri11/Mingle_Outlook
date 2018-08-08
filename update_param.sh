d01326e6d16482e26fd6c004b02084902c1386d5
Ankit <ankitsri11@users.noreply.github.com>
2018-08-08 15:57:35 -0700

    Update update_param.sh

--- update_param.sh
+++ update_param.sh
@@ -5,7 +5,7 @@
 set -e
 
 
-curl -s -H 'Accept: application/vnd.go.cd.v6+json' -D headers -o pipeline 'http://localhost:8153/go/api/admin/pipelines/PipelineB'
+curl -s -H 'Accept: application/vnd.go.cd.v6+json' -D headers -o pipeline 'http://172.17.0.2:8153/go/api/admin/pipelines/PipelineB'
 ETAG=$(sed -n 's/^ETag: "\([^"]*\)".*/\1/p' headers)
 
 echo $ETAG
@@ -15,6 +15,6 @@ sed -e 's/"label_template" : "/&'$release_type'-/' pipeline >pipeline.for_update
 
 cat pipeline.for_update
 
-curl -s -o pipeline.after_update -X PUT -H 'Content-Type: application/json' -H "If-Match: \"$ETAG\"" -H 'Accept: application/vnd.go.cd.v6+json' --data-binary @pipeline.for_update 'http://localhost:8153/go/api/admin/pipelines/PipelineB'
+curl -s -o pipeline.after_update -X PUT -H 'Content-Type: application/json' -H "If-Match: \"$ETAG\"" -H 'Accept: application/vnd.go.cd.v6+json' --data-binary @pipeline.for_update 'http://172.17.0.2:8153/go/api/admin/pipelines/PipelineB'
 
 echo "Updated pipeline config is in the file pipeline.for_update"
